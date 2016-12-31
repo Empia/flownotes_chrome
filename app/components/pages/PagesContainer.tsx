1635d2dd3cad1da3065ded2a9fae85d4f6c96c1f
Stan <iamjacke@gmail.com>
2016-12-31 20:49:31 +0500

    strange bug fixing

--- app/components/pages/PagesContainer.tsx
+++ app/components/pages/PagesContainer.tsx
@@ -4,7 +4,8 @@ import PagesSidebar from './PagesSidebar';
 import { connect } from 'react-redux';
 //import { EmailSignUpForm } from "redux-auth/default-theme";
 //import * as EmailSignUpForm from "redux-auth/src/views/default/index.js";
-
+//import * as Test from '../../redux-auth/src/index';
+//const redux_immutablejs_1 = require("redux-immutablejs");
 
 interface PagesContainerProps extends React.Props<any>{
   key:string;
@@ -13,9 +14,8 @@ interface PagesContainerState{ }
 
 class PagesContainer extends React.Component<PagesContainerProps, PagesContainerState>{
     constructor(){
-    super();
-    console.log('EmailSignUpForm', null);
-  }
+      super();
+    }
   
   render(){
     return  (
