ec04e910ef7d005e63aec4a8d62f22dc4b48448f
Stan <iamjacke@gmail.com>
2016-12-31 20:57:47 +0500

    qqq

--- app/components/pages/PagesContainer.tsx
+++ app/components/pages/PagesContainer.tsx
@@ -2,10 +2,6 @@ import * as React from "react";
 import Header from '../commons/Header';
 import PagesSidebar from './PagesSidebar';
 import { connect } from 'react-redux';
-//import { EmailSignUpForm } from "redux-auth/default-theme";
-//import * as EmailSignUpForm from "redux-auth/src/views/default/index.js";
-//import * as Test from '../../redux-auth/src/index';
-//const redux_immutablejs_1 = require("redux-immutablejs");
 
 interface PagesContainerProps extends React.Props<any>{
   key:string;
