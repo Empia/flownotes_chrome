876ea60d816cf885420fb3fb7d9aab7b5870c8d1
Stan <iamjacke@gmail.com>
2017-02-13 00:17:11 +0500

    Install test enviorment Completed

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,4 +1,6 @@
 var context = require.context('./app', true, /-test\.jsx$/);
 context.keys().forEach(context);
+/*
 var context2 = require.context('./app', true, /-test\.tsx$/);
 context2.keys().forEach(context2);
+*/
