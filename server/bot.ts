7aadcac94a82528246811310b120aa803fb9e8ec
Stan <iamjacke@gmail.com>
2017-03-18 19:25:31 +0500

    basis for bot was implemented

--- server/bot.ts
+++ server/bot.ts
@@ -1,4 +1,5 @@
 var TelegramBot = require('node-telegram-bot-api');
+import commands from './chat_bot_core/commands';
 
 const bot = {
   initiate: () => {
@@ -6,8 +7,18 @@ const bot = {
     const bot = new TelegramBot(token, {workers: 1, polling: true});
     bot.on('message', function (msg) {
         var chatId = msg.chat.id;
-        console.log(msg);
-        bot.sendMessage(chatId, msg.text.toString()+' fuck bitches', {caption: "I'm a bot!"});
+        //console.log(msg);
+        var commandOpt = commands.resolveCmd(msg.text.toString());
+        if (commandOpt.resolved) {
+          var doneCmd = commands[commandOpt.cmd](msg)
+          var response:string = doneCmd.response
+          var responseCaption:string = doneCmd.caption ? doneCmd.caption : ''
+          bot.sendMessage(chatId, response, {caption: responseCaption});
+        } else {
+          bot.sendMessage(chatId, msg.text.toString()+' fuck bitches', {caption: "I'm a bot!"});
+        }
+
+
     });
     bot.on('polling_error', (error) => {
       console.log(error);  // => 'EFATAL'
