8c85ffd5c780cbb6edfa88244e8f278a9a0193e4
Stan <iamjacke@gmail.com>
2017-03-21 02:32:44 +0500

    fix first run

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 module.exports = function (config) {
     config.set({
         frameworks: ["mocha", "karma-typescript"],
+        singleRun: true,
         files: [
             { pattern: "node_modules/expect.js/index.js" },
             { pattern: "app/components/__tests__/*.tsx" }
