4124271b7917410c65d6376ea10c22089c48c1d2
Stan <iamjacke@gmail.com>
2017-11-26 13:49:41 +0300

    Repository example was added

---
+++ DESCRIPTION.md
@@ -0,0 +1,21 @@
+# Description
+
+/*
+> url > operation
+
+
+
+> shoutbox > urls > to collections :: to activity tags
+> collections > urls
+> activities > urls
+
+url >
+> save to collection
+> later to shoutbox
+> later to activity
+
+activity > brief description when you need to use this url
+
+*/
+
+
--- new_bot.js
+++ new_bot.js
@@ -77,7 +77,6 @@ app.on('text', (ctx, next) => {
   return next()
 })
 
-app.use(flow.middleware())
 app.use(commandParts());
 app.startPolling()
 
@@ -114,90 +113,3 @@ console.log('NOTER IS RUNNING');
 console.log('***************');
 console.log('***************');
 console.log('database', database);
-
-
-
-
-var mongoose = require('mongoose');
-var repository = function (modelName) {
-  var self = this;
-  self.Model = require('../models/' + modelName);
-  self.FindById = function (id, cb) {
-    self.FindOne({
-      _id : id
-    }, function(err, entity) {
-      cb(err, entity);
-    });
-  };
-
-  self.FindOne = function (params, cb) {
-    self.Model.findOne(params, function (err, entity) {
-      if (!err && !entity) {
-        err = true;
-      }
-
-      cb(err, entity);
-    });
-  };
-
-  self.FindAll = function (params, cb, lean) {
-    if (!lean) {
-        self.Model.find(params).exec(cb);
-    } else {
-        self.Model.find(params).lean().exec(cb);
-    }
-  };
-
-  self.Save = function (obj, cb) {
-    var entity = new self.Model(obj);
-    entity.save(function (err) {
-      cb(err);
-    });
-  };
-
-  self.Update = function (entity, cb) {
-    self.FindById(entity.id, function (err, oldEntity) {
-      if (err) {
-        cb(err);
-      } else {
-          oldEntity = entity;
-          oldEntity.save(cb);
-      }
-    })
-  };
-
-  self.Delete = function (entity, cb) {
-    entity.remove(function (err) {
-      cb(err);
-    });
-  };
-};
-
-repository.GetModel = function (modelName) {
-  return mongoose.model(modelName);
-};
-
-module.exports.Repository = repository;
-
-
-
-
-/*
-> url > operation
-
-
-
-> shoutbox > urls > to collections :: to activity tags
-> collections > urls
-> activities > urls
-
-url >
-> save to collection
-> later to shoutbox
-> later to activity
-
-activity > brief description when you need to use this url
-
-*/
-
-
