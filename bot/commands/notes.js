b1a931d9e4b8f5b8de7db5ceada02c1eeacdf21e
Stan <iamjacke@gmail.com>
2017-11-27 16:13:24 +0300

    NOTE CR

--- bot/commands/notes.js
+++ bot/commands/notes.js
@@ -2,12 +2,20 @@ const { Markup, reply } = require('telegraf')
 const commandParts = require('telegraf-command-parts');
 const TelegrafFlow = require('telegraf-flow');
 const { Scene, enter, leave } = TelegrafFlow
+const {getMetadata, metadataRules} = require('page-metadata-parser');
+var MetaInspector = require('node-metainspector');
+const normalizeUrl = require('normalize-url');
+
+import {removeNote,
+findNote,
+findNotes,
+saveNote, NoteModel} from '../models/Notes';
 
 const noteScene = new Scene('notes');
 
 const keyboard = Markup
     .keyboard([
-      ['Коллекции', 'Активность'], // '☸ Setting',  Row2 with 2 buttons
+      ['Свежие', 'Коллекции', 'Активность'], // '☸ Setting',  Row2 with 2 buttons
       ['Назад']
     ])
     .oneTime()
@@ -21,8 +29,54 @@ noteScene.leave((ctx) => {
 	ctx.reply(`Вы добавили ${5} ссылок`);
 });
 
+
+
+const addNoteByUrl = (url, userId) => {
+	console.log('addNoteByUrl', url);
+		 return {title: "client.title",
+						  content_desc:'',
+						  content_type: 'url',
+						  content_value: url,
+						  inPageId:'',
+						  inContent:'',
+						  labels: [],
+						  userId: userId,
+						  userProvider: 'telegram'};
+};
+
+
+//removeNote = ((id) =
+//findNote = ((id) =
+
+
+
+noteScene.hears('Свежие', (ctx) => {
+	findNotes().then((notes) => {
+		notes.map(note => ctx.reply(note));
+	})
+})
+
+
+noteScene.hears('Назад', (ctx) => {
+	ctx.flow.enter('start');
+});
+
+
+
+
 noteScene.on('message', (ctx) => {
-  ctx.flow.enter('start');
+  console.log('onMessage', ctx.message.text);
+  let telegramLogin =  ctx.chat.username
+  let url = ctx.message.text 
+  if (url !== undefined && url !== "") {
+	  saveNote(addNoteByUrl(url, telegramLogin)).then((c) => {
+	  	  ctx.reply('Added')
+
+	  })
+  } else {
+  	  ctx.reply('Incorrect url')
+  }
+
 });
 
 export default noteScene;
--- bot/models/Shoutboxes.js
+++ bot/models/Shoutboxes.js
@@ -4,17 +4,6 @@ var Schema = mongoose.Schema;
 
 const schema = new Schema({ 
   title:String,
-  content_desc:String,
-  content_type: String,
-  content_value: String,
-  inPageId:String,
-  inContent:String,
-  labels:{ type: Array, default: []};// <ILabels>;
-  userId:String;
-  userProvider: String;
-  meta:{ type: Array, default: []},// <IMeta>;
-  states:{ type: Array, default: []},// <IConState>;
-  order: { type: Number, default: 0 },  
   createdAt: { type: Date, default: Date.now},
   updatedAt: { type: Date, default: Date.now}
  });
